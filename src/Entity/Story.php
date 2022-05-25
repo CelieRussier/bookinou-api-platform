827d5822b0c156b112bef3783359c620b0fd9be3
Celie Russier <celie.russier@gmail.com>
2022-05-25 16:37:28 +0200

    with all the files

---
+++ src/Entity/Story.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\StoryRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
+#[ORM\Entity(repositoryClass: StoryRepository::class)]
+#[ApiResource()]
+class Story
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $title;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private $description;
+
+    #[ORM\Column(type: 'string', length: 13, nullable: true)]
+    private $ean;
+
+    public function __construct()
+    {
+        $ean = 97;
+        $ean .= rand(8, 9);
+        for ($i=0; $i<10; $i++) {
+            $ean .= rand(0,9);
+        }
+        $this->ean = $ean;
+    }
+    
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getEan(): ?string
+    {
+        return $this->ean;
+    }
+
+    public function setEan(?string $ean): self
+    {
+        $this->ean = $ean;
+
+        return $this;
+    }
+}
